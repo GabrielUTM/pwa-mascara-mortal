374600c2ed8a60fd17671c141e49d9cf0e5cfcf1
Gabriel Ramirez <utm210058ti@ut-morelia.edu.mx>
2024-12-07 16:21:45 -0600

    service worker agregado

--- app/index.js
+++ app/index.js
@@ -1,4 +1,16 @@
 import { App } from "./App.js";
 
 document.addEventListener("DOMContentLoaded", App);
+
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("./service-worker.js")
+    .then(function (registration) {
+      console.log("Service worker registrado con exito:", registration);
+    })
+    .catch(function (error) {
+      console.log("Error al registrar el service worker:", error);
+    });
+}
+
 window.addEventListener("hashchange", App);
---
+++ app/service-worker.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = "mascara-mortal-pwa-cache-v1";
+
+const urlsToCache = ["./", "../index.html", "./app/App.js", "./app/index.js"];
+
+// Evento de instalación
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Abriendo caché");
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Evento de activación
+self.addEventListener("activate", (event) => {
+  console.log("Service Worker activado");
+});
+
+// evento de fetch para interceptar solicitudes y servir desde caché
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
